676433a5ba60092fff846681b0dc09e9dbd99b58
derrickkim <olaaivl@naver.com>
2020-12-27 17:53:35 +0900

    Camera_Gallery_Network

--- Hay/app/src/main/java/com/android/mypeople/CUDNetworkTask.java
+++ Hay/app/src/main/java/com/android/mypeople/CUDNetworkTask.java
@@ -5,68 +5,206 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
-public class CUDNetworkTask extends AsyncTask<Integer, String, Void> {
+public class CUDNetworkTask extends AsyncTask<Integer, String, Object> {
 
-    /*공용이므로 항상 수정사항 알려주시기 바랍니다.*/
-
-    final static String TAG = "CUDNetworkTask";
+    final static String TAG = "NetworkTask";
     Context context = null;
     String mAddr = null;
-//    ProgressDialog progressDialog = null;
+    ProgressDialog progressDialog = null;
+    //    ArrayList<Student> members;
+    ///////////////////////////////////////////////////////////////////////////////////////
+    // Date : 2020.12.25
+    //
+    // Description:
+    //  - NetworkTask를 검색, 입력, 수정, 삭제 구분없이 하나로 사용키 위해 생성자 변수 추가.
+    //
+    ///////////////////////////////////////////////////////////////////////////////////////
+    String where = null;
 
-    public CUDNetworkTask(Context context, String mAddr) {
+    public CUDNetworkTask(Context context, String mAddr, String where) {
         this.context = context;
         this.mAddr = mAddr;
-        Log.v(TAG,"Start : " + mAddr);
+//        this.members = new ArrayList<Student>();
+        this.where = where;
+        Log.v(TAG, "Start : " + mAddr);
     }
 
     @Override
     protected void onPreExecute() {
         Log.v(TAG, "onPreExecute()");
-//        progressDialog = new ProgressDialog(context);
-//        progressDialog.setProgressStyle(progressDialog.STYLE_SPINNER);
-//        progressDialog.setTitle("Create/Update/Delete");
-//        progressDialog.setMessage("Working...");
-//        progressDialog.show();
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setTitle("Dialogue");
+        progressDialog.setMessage("Get ....");
+        progressDialog.show();
+
     }
 
     @Override
-    protected Void doInBackground(Integer... integers) {
-        Log.v(TAG, "doInBackground");
-        try{
+    protected Object doInBackground(Integer... integers) {
+        Log.v(TAG, "doInBackground()");
+
+        StringBuffer stringBuffer = new StringBuffer();
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+        ///////////////////////////////////////////////////////////////////////////////////////
+        // Date : 2020.12.25
+        //
+        // Description:
+        //  - NetworkTask에서 입력,수정,검색 결과값 관리.
+        //
+        ///////////////////////////////////////////////////////////////////////////////////////
+        String result = null;
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+        try {
             URL url = new URL(mAddr);
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.setConnectTimeout(10000);
-            if(httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+
+            if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                inputStream = httpURLConnection.getInputStream();
+                inputStreamReader = new InputStreamReader(inputStream);
+                bufferedReader = new BufferedReader(inputStreamReader);
+
+                while (true) {
+                    String strline = bufferedReader.readLine();
+                    if (strline == null) break;
+                    stringBuffer.append(strline + "\n");
+                }
+                ///////////////////////////////////////////////////////////////////////////////////////
+                // Date : 2020.12.25
+                //
+                // Description:
+                //  - 검색으로 들어온 Task는 parserSelect()로
+                //  - 입력, 수정, 삭제로 들어온 Task는 parserAction()으로 구분
+                //
+                ///////////////////////////////////////////////////////////////////////////////////////
+                if (where.equals("select")) {
+//                    parserSelect(stringBuffer.toString());
+                } else {
+                    result = parserAction(stringBuffer.toString());
+                }
+                ///////////////////////////////////////////////////////////////////////////////////////
 
             }
-
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null) bufferedReader.close();
+                if (inputStreamReader != null) inputStreamReader.close();
+                if (inputStream != null) inputStream.close();
+
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
         }
+        ///////////////////////////////////////////////////////////////////////////////////////
+        // Date : 2020.12.25
+        //
+        // Description:
+        //  - 검색으로 들어온 Task는 members를 return
+        //  - 입력, 수정, 삭제로 들어온 Task는 result를 return
+        //
+        ///////////////////////////////////////////////////////////////////////////////////////
+//        if (where.equals("select")) {
+////            return members;
+//        } else {
+            return result;
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////
 
-        return null;
-    }
+
+
 
     @Override
-    protected void onProgressUpdate(String... values) {
-        Log.v(TAG, "onProgressUpdate()");
-        super.onProgressUpdate(values);
-    }
-
-    @Override
-    protected void onPostExecute(Void aVoid) {
+    protected void onPostExecute(Object o) {
         Log.v(TAG, "onPostExecute()");
-        super.onPostExecute(aVoid);
-//        progressDialog.dismiss();
+        super.onPostExecute(o);
+        progressDialog.dismiss();
+
     }
 
     @Override
     protected void onCancelled() {
-        Log.v(TAG, "onCancelled");
+        Log.v(TAG,"onCancelled()");
         super.onCancelled();
     }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    // Date : 2020.12.25
+    //
+    // Description:
+    //  - 검색후 json parsing
+    //
+    ///////////////////////////////////////////////////////////////////////////////////////
+//    private void parserSelect(String s){
+//        Log.v(TAG,"Parser()");
+//
+//        try {
+//            JSONObject jsonObject = new JSONObject(s);
+//            JSONArray jsonArray = new JSONArray(jsonObject.getString("students_info"));
+//            members.clear();
+//
+//            for(int i = 0; i < jsonArray.length(); i++){
+//                JSONObject jsonObject1 = (JSONObject) jsonArray.get(i);
+//                String code = jsonObject1.getString("code");
+//                String name = jsonObject1.getString("name");
+//                String dept = jsonObject1.getString("dept");
+//                String phone = jsonObject1.getString("phone");
+//
+//                Log.v(TAG, "code : " + code);
+//                Log.v(TAG, "name : " + name);
+//                Log.v(TAG, "dept : " + dept);
+//                Log.v(TAG, "phone : " + phone);
+//
+//                Bean_friendslist member = new Student(code, name, dept, phone);
+//                members.add(member);
+//                // Log.v(TAG, member.toString());
+//                Log.v(TAG, "----------------------------------");
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+
+    ///////////////////////////////////////////////////////////////////////////////////////
+    // Date : 2020.12.25
+    //
+    // Description:
+    //  - 입력, 수정, 삭제후 json parsing
+    //
+    ///////////////////////////////////////////////////////////////////////////////////////
+    private String parserAction(String s){
+        Log.v(TAG,"Parser()");
+        String returnValue = null;
+
+        try {
+            Log.v(TAG, s);
+
+            JSONObject jsonObject = new JSONObject(s);
+            returnValue = jsonObject.getString("result");
+            Log.v(TAG, returnValue);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return returnValue;
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////
+
 } // ----------
